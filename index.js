c7e288f80c43e9de5dd2092eb508b039348eafe0
Mahendra <mahendrakshirsagar176@gmail.com>
2024-04-22 07:43:07 +0530

    seperated frontend part

--- index.js
+++ index.js
@@ -9,15 +9,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(express.json());
-app.use(express.static('public'));
-app.use(express.static('dist'));
 app.use(router);
-
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
 
 mongoose.connect(process.env.DB_URL);
 
